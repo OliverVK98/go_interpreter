4911b5a720f4a5f22fe577ed997c1b54c2471b3a
OliverVK98 <v1155533@gmail.com>
2024-01-24 23:31:41 -0500

    Added return&expression parsers

--- parser/parser_test.go
+++ parser/parser_test.go
@@ -110,3 +110,34 @@ func checkParsesErrors(t *testing.T, p *Parser) {
 	t.FailNow()
 
 }
+
+func TestIdentifierExpression(t *testing.T) {
+	input := "foobar;"
+
+	l := lexer.New(input)
+	p := New(l)
+	program := p.ParseProgram()
+	checkParsesErrors(t, p)
+
+	if len(program.Statements) != 1 {
+		t.Fatalf("program has incorrect number of statements. got = %v", len(program.Statements))
+	}
+
+	stmt, ok := program.Statements[0].(*ast.ExpressionStatement)
+	if !ok {
+		t.Fatalf("program.Statements[0] is not ast.ExpressionStatement. got = %v", program.Statements[0])
+	}
+
+	ident, ok := stmt.Expression.(*ast.Identifier)
+	if !ok {
+		t.Fatalf("exp not *ast.Identifier. got = %v", stmt.Expression)
+	}
+
+	if ident.Value != "foobar" {
+		t.Errorf("ident.Value not %v. got %v", "foobar", ident.Value)
+	}
+
+	if ident.TokenLiteral() != "foobar" {
+		t.Errorf("ident.TokenLiteral not %v. got %v", "foobar", ident.TokenLiteral())
+	}
+}
